d190bc0e0c847aaa1f2ec11ea603180f90d40bf4
unknown <Selvenchi>
2023-07-03 17:01:15 +0700

    hasil instagram clone

---
+++ instagram.js
@@ -0,0 +1,69 @@
+console.log("Hello")
+
+// String -> TEXT "hello"
+// Integer -> ANGKA 12
+// Booelan -> true/false
+
+const tulisan = "hallo ini text panjang banget"
+
+console.log("hallo ini text panjang banget")
+
+// console.log(tulisan)
+// console.log(tulisan)
+// console.log(tulisan)
+// console.log(tulisan)
+// console.log(tulisan)
+
+
+const Tulisan2 = "apa aja"
+
+console.log(Tulisan2)
+
+// namaVariable, iniTulisan, halloNamaSayaNama (camel case)
+
+const angka1 = 10;
+const angka2 = 20
+
+console.log(angka1 * angka2)
+
+// insta
+
+const likeButton = document.querySelector(".fa-heart-1")
+
+console.log(likeButton)
+
+likeButton.addEventListener("click", () => {
+  likeButton.classList.toggle("active")
+})
+
+//  bookmark
+
+const saveButton = document.querySelector(".fa-bookmark-1")
+
+console.log(saveButton)
+
+saveButton.addEventListener("click", () => {
+  saveButton.classList.toggle("active")
+})
+
+// lol
+
+const shareButton = document.querySelector(".fa-share-1")
+
+console.log(shareButton)
+
+shareButton.addEventListener ("click", () => {
+  console.log(tulisan)
+})
+
+// lol
+
+const commentButton = document.querySelector(".fa-comment-1")
+
+console.log(commentButton)
+
+commentButton.addEventListener("click", () => {
+  console.log(Tulisan2)
+})
+
+// 
